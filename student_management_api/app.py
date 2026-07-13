2eccd928fc22c9d196ac91b46e82ec87202d295a
Vaishnavi Kogalla <vaishnavikongalla7@gmail.com>
2026-07-13 23:06:42 +0530

    Add files via upload

---
+++ student_management_api/app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud
+import schemas
+from database import SessionLocal, engine, Base
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@app.post("/students")
+def create_student(
+    student: schemas.StudentCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_student(db, student)
+
+
+@app.get("/students")
+def get_students(
+    db: Session = Depends(get_db)
+):
+    return crud.get_students(db)
+
+
+@app.get("/students/{student_id}")
+def get_student(
+    student_id: int,
+    db: Session = Depends(get_db)
+):
+    student = crud.get_student(db, student_id)
+    if student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+@app.put("/students/{student_id}")
+def update_student(
+    student_id: int,
+    student: schemas.StudentCreate,
+    db: Session = Depends(get_db)
+):
+    updated_student = crud.update_student(db, student_id, student)
+    if updated_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    return updated_student
+@app.delete("/students/{student_id}")
+def delete_student(
+    student_id: int,
+    db: Session = Depends(get_db)
+):
+    deleted_student = crud.delete_student(db, student_id)
+    if deleted_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return {
+        "message": "Student deleted successfully"
+    }
---
+++ student_management_api/crud.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from models import Student
+from schemas import StudentCreate
+def create_student(db: Session, student: StudentCreate):
+    db_student = Student(
+        name=student.name,
+        age=student.age,
+        course=student.course
+    )
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+def get_students(db: Session):
+    return db.query(Student).all()
+def get_student(db: Session, student_id: int):
+    return db.query(Student).filter(Student.id == student_id).first()
+def update_student(db: Session, student_id: int, student: StudentCreate):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        return None
+    db_student.name = student.name
+    db_student.age = student.age
+    db_student.course = student.course
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+def delete_student(db: Session, student_id: int):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        return None
+    db.delete(db_student)
+    db.commit()
+    return db_student
